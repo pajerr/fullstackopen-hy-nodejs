adc84a08fa394a2aa8dc5d720047f6c95d55bfcb
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-11 17:49:28 +0200

    3.12 done

--- phonebook-backend/mongo.js
+++ phonebook-backend/mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+
 //get password from cli arguments
 const password = process.argv[2]
 
@@ -11,33 +12,31 @@ if (process.argv.length < 3) {
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.isxty0d.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-//The schema tells Mongoose how the note objects are to be stored in the database.
-/*
-const noteSchema = new mongoose.Schema({
-    content: String,
-    date: Date,
-    important: Boolean
-})
-*/
 const personSchema = new mongoose.Schema({
     name: String,
     number: String,
     id: Number
 })
 
-//Mongoose automatically name collections as the plural (e.g. notes)
-//const Note = mongoose.model('Note', noteSchema)
 const Person = mongoose.model('Person', personSchema)
 
 mongoose.connect(url).then((result) => {
-    console.log('connected')
+    if (process.argv.length === 3) {
+        console.log('phonebook:')
+        Person.find({}).then((result) => {
+            result.forEach((person) => {
+                console.log(`${person.name} ${person.number}`)
+            })
+            mongoose.connection.close()
+        })
+    }
 
     //if there are 3 arguments, then add a new person
     if (process.argv.length === 5) {
         const person = new Person({
             name: process.argv[3],
             number: process.argv[4],
-            id: Math.floor(Math.random() * 1000000)
+            id: Math.floor(Math.random() * 999999)
         }) //close const person
 
         return person
