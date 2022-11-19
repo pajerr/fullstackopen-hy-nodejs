46c05d89627892f5330214282ae4a0d913e84483
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-19 16:41:29 +0200

    part 3 phonebook backend finished

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -13,21 +13,21 @@ app.use(express.json())
 app.use(middleware)
 
 app.get('/api/persons', (request, response) => {
-    //response.json(persons)
-    Person.find({}).then((persons) => {
-        response.json(persons)
-    })
+  //response.json(persons)
+  Person.find({}).then((persons) => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
-    const date = new Date()
-    response.send(
-        `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
-    )
+  const date = new Date()
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`
+  )
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    /*const id = Number(request.params.id)
+  /*const id = Number(request.params.id)
     const person = persons.find((person) => person.id === id)
 
     if (person) {
@@ -35,84 +35,84 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }*/
-    Person.findById(request.params.id).then((person) => {
-        response.json(person)
-    })
+  Person.findById(request.params.id).then((person) => {
+    response.json(person)
+  })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    /*const id = Number(request.params.id)
+  /*const id = Number(request.params.id)
     persons = persons.filter((person) => person.id !== id)
 
     response.status(204).end()*/
-    Person.findByIdAndRemove(request.params.id)
-        .then((result) => {
-            response.status(204).end()
-        })
-        .catch((error) => next(error))
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end()
+    })
+    .catch((error) => next(error))
 })
 
 const generateId = () => {
-    min = Math.ceil(9)
-    max = Math.floor(19352)
-    return Math.floor(Math.random() * (max - min) + min)
+  min = Math.ceil(9)
+  max = Math.floor(19352)
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
 app.post('/api/persons', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    if (!body.name) {
-        return response.status(400).json({
-            error: 'name missing'
-        })
-    }
-    if (!body.number) {
-        return response.status(400).json({
-            error: 'number missing'
-        })
-    }
-    /*
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+  /*
     if (persons.find((person) => person.name === body.name)) {
         return response.status(400).json({
             error: 'name must be unique'
         })
     }*/
-    /*
+  /*
     morgan_log = morgan.token('type', function (req, res) {
         return req.body.name
     })
     */
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  })
 
-    //console.log(morgan_log)
-    person.save().then((savedPerson) => {
-        response.json(savedPerson)
-    })
+  //console.log(morgan_log)
+  person.save().then((savedPerson) => {
+    response.json(savedPerson)
+  })
 })
 
 //Unknown endpoint middleware must be the 2nd last loaded middleware, before the error handler.
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+  console.error(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-    next(error)
+  next(error)
 }
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- phonebook-backend/models/person.js
+++ phonebook-backend/models/person.js
@@ -5,26 +5,26 @@ const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 
 mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-    id: Number
+  name: String,
+  number: String,
+  id: Number
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
