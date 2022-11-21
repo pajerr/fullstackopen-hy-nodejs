aa7804126eaa215019f06b61b80ec7a08335d871
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-21 20:56:32 +0200

    4.2 wip

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,9 +2,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-//const Blog = mongoose.model('Blog', blogSchema)
-//to be mobved to controllers
-const Blog = require('./models/blog')
+
+//these are to be moved to the app
+const blogsRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogsRouter)
 
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -13,20 +14,6 @@ mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
