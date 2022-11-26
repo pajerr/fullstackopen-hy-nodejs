6bcbd246c9fbe88bd54a8b5b61fcbcdb14d245c6
Erno <54817547+pajerr@users.noreply.github.com>
2022-11-26 13:45:58 +0200

    4.4 done

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,17 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes))
+  return blogs.find((blog) => blog.likes === maxLikes)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
