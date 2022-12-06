f9cd64067c28c499cda51b4935bdfd39fc7e3e4e
Erno <54817547+pajerr@users.noreply.github.com>
2022-12-06 19:36:29 +0200

    4.8 done

--- part4/blog/controllers/blogs.js
+++ part4/blog/controllers/blogs.js
@@ -1,18 +1,24 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
+blogsRouter.get('/', async (request, response) => {
+  //promise version replaced by async & await
+  // Blog.find({}).then((blogs) => {
+  //   response.json(blogs)
+  // })
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
+  // const blog = new Blog(request.body)
+  // blog.save().then((result) => {
+  //   response.status(201).json(result)
+  // })
   const blog = new Blog(request.body)
+  const result = await blog.save()
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
+  response.status(201).json(result)
 })
 
 module.exports = blogsRouter
