8d9ac6596d74ede34822ab8aaba247af0af65a88
Erno <54817547+pajerr@users.noreply.github.com>
2022-12-06 20:06:21 +0200

    4.9 done

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -24,7 +24,15 @@ test('blog entries are returned in JSON format', async () => {
 
 test('2 blog posts are returned', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
+  //expect(response.body).toHaveLength(helper.initialBlogs.length)
+  for (let blog of response.body) {
+    expect(blog.id).toBeDefined()
+  }
+})
+
+test('blog entries have id property', async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body[0].id).toBeDefined()
 })
 
 afterAll(() => {
