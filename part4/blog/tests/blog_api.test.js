4982c6b04f81b83ca13b807708ea1e412ca5125d
Erno <54817547+pajerr@users.noreply.github.com>
2022-12-11 11:37:19 +0200

    4.13 done

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -93,6 +93,21 @@ test('it responds 400 with title or url is missing', async () => {
   await api.post('/api/blogs').send(BlogWithoutURL).expect(400)
 })
 
+test('a blog can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb()
+  const blogToDelete = blogsAtStart[0]
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204)
+
+  const blogsAtEnd = await helper.blogsInDb()
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1)
+
+  const contents = blogsAtEnd.map((blog) => blog.title)
+
+  expect(contents).not.toContain(blogToDelete.title)
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
